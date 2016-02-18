30ec20a005b7770d75cd60748980a4b401007496
Brian Stumbaugh <brian.stumbuagh@gmail.com>
2016-02-18 10:36:17 -0600

    first feature done

--- server.js
+++ server.js
@@ -1,14 +1,31 @@
 'use strict';
 
-
+const bodyParser = require('body-parser');
 const express = require('express');
+
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.set('view engine', 'jade');
+app.use(bodyParser.urlencoded({
+	extended:false
+}))
+
 app.get('/', (req, res) => {
 	res.send('server running');
 });
+
+// this route will just serve up the form
+app.get('/notes/new', (req, res) => {
+	res.render('new-note');
+});
+
+app.post('/notes', (req, res) => {
+	console.log(req.body);
+	res.redirect('/');
+});
+
 
 
 app.listen(port, () => {
